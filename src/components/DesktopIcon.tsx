38d0dcfa6cc1f801b8a2935108489cc3bc0df2ff
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-05-22 17:47:50 +0200

    fix icon text

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -17,15 +17,19 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          gap: 8,
+          gap: 4,
           textAlign: "center",
           cursor: "pointer",
           userSelect: "none",
           margin: 10,
           textShadow:
             "-1px -1px 0 black, \
+             0px -1px 0 black, \
              1px -1px 0 black, \
+             -1px  0px 0 black, \
+             1px  0px 0 black, \
              -1px  1px 0 black, \
+             0px  1px 0 black, \
              1px  1px 0 black;",
           color: "white",
         }}
