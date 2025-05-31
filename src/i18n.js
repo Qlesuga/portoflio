f691984823e52150544ee213924c1ce07139aeaf
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-06-01 00:23:34 +0200

    i18n config

--- src/i18n.js
+++ src/i18n.js
@@ -14,7 +14,76 @@ i18n
     resources: {
       en: {
         translation: {
-          test: "test from i18",
+          skills: {
+            title: "Skills",
+            proficient: {
+              title: "Proficient Programming Languages",
+              description:
+                "Languages I've used in larger projects with hands-on experience",
+            },
+            familiar: {
+              title: "Familiar Programming Languages",
+              description:
+                "Languages I’ve played around with in smaller projects",
+            },
+            libaries: {
+              title: "Libraries/Frameworks",
+              description:
+                "Stuff I use to build things on the front-end, back-end, styling, you name it",
+            },
+            tools: {
+              title: "Software/Tools",
+              description:
+                "Tools I use all the time—whether it’s for coding, designing, running apps, or managing data",
+            },
+          },
+          contact: {
+            title: "Contact Form",
+            description:
+              "Have a question or want to get in touch? Fill out the form below or contact me directly, and I will get back to you as soon as possible.",
+            form: {
+              nameLabel: "Name",
+              messageLabel: "Message",
+              sendMessage: "Send Message",
+            },
+          },
+        },
+      },
+      pl: {
+        translation: {
+          skills: {
+            title: "Umiejętności",
+            proficient: {
+              title: "Języki Programowania w których jestem biegły",
+              description:
+                "Języki, których używałem w większych projektach i mam z nimi praktyczne doświadczenie",
+            },
+            familiar: {
+              title: "Języki, z którymi miałem Styczność",
+              description:
+                "Języki, z którymi miałem styczność w mniejszych projektach",
+            },
+            libaries: {
+              title: "Biblioteki/Frameworki",
+              description:
+                "Rzeczy, których używam do tworzenia aplikacji front-end, back-end, stylowanie, co sobie wymyśle",
+            },
+            tools: {
+              title: "Oprogramowanie/Narzędzia",
+              description:
+                "Narzędzia, z których regularnie korzystam — do kodowania, projektowania, uruchamiania aplikacji czy zarządzania danymi",
+            },
+          },
+          contact: {
+            title: "Formularz Kontaktowy",
+            description:
+              "Masz pytanie lub chcesz się skontaktować? Wypełnij poniższy formularz lub skontaktuj się ze mną bezpośrednio, a odezwę się tak szybko, jak to możliwe.",
+            form: {
+              nameLabel: "Nazwa",
+              messageLabel: "Wiadomość",
+              sendMessage: "Wyślij Wiadomość",
+            },
+          },
         },
       },
     },
