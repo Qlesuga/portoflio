68ff406437c8e00f9eb82ec6d7d9dd18f35e1777
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2025-06-02 14:08:59 +0200

    draggable desktop icons

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -1,3 +1,10 @@
+import { useEffect, useRef, useState, type MouseEvent } from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
 interface DesktopIconProps {
   createDraggableWindow: (title: string, children: React.ReactNode) => void;
   selectIcon: (name: string) => void;
@@ -13,23 +20,76 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({
   selectIcon,
   isSelected = false,
 }) => {
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+  const initialPosition = useRef<Position | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const dragging = useRef(false);
+  const rel = useRef<Position>({ x: 0, y: 0 });
+  const windowRef = useRef<HTMLDivElement | null>(null);
+
+  const onMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+    if (
+      e.button !== 0 ||
+      !windowRef.current ||
+      !initialPosition.current ||
+      isDragging
+    )
+      return;
+    const rect = windowRef.current.getBoundingClientRect();
+    dragging.current = true;
+    setIsDragging(true);
+    rel.current = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top,
+    };
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+    e.preventDefault();
+  };
+
+  const onMouseMove = (e: MouseEvent | globalThis.MouseEvent) => {
+    if (!dragging.current) return;
+    setPosition({
+      x: e.clientX - rel.current.x - initialPosition.current!.x,
+      y: e.clientY - rel.current.y - initialPosition.current!.y,
+    });
+  };
+
+  const onMouseUp = () => {
+    dragging.current = false;
+    setIsDragging(false);
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  };
+
+  useEffect(() => {
+    const rect = windowRef.current?.getBoundingClientRect();
+    if (!rect) return;
+    initialPosition.current = {
+      x: rect.x,
+      y: rect.y,
+    };
+  }, []);
   return (
-    <>
-      <div
-        style={{
-          width: 90,
-          height: 90,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 6,
-          textAlign: "center",
-          cursor: "pointer",
-          userSelect: "none",
-          padding: 4,
-          textShadow:
-            "-1px -1px 0 black, \
+    <div
+      ref={windowRef}
+      style={{
+        width: 90,
+        height: 90,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 6,
+        textAlign: "center",
+        cursor: "pointer",
+        userSelect: "none",
+        position: "relative",
+        top: position.y,
+        left: position.x,
+        padding: 4,
+        textShadow:
+          "-1px -1px 0 black, \
              0px -1px 0 black, \
              1px -1px 0 black, \
              -1px  0px 0 black, \
@@ -37,32 +97,35 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({
              -1px  1px 0 black, \
              0px  1px 0 black, \
              1px  1px 0 black",
-          color: "white",
-          backgroundColor: isSelected
-            ? "rgba(255, 255, 255, 0.2)"
-            : "transparent",
+        color: "white",
+        backgroundColor: isSelected
+          ? "rgba(255, 255, 255, 0.2)"
+          : "transparent",
+      }}
+      onDoubleClick={() => createDraggableWindow("test", <p>test</p>)}
+      onMouseDown={(e) => {
+        selectIcon(name);
+        onMouseDown(e);
+      }}
+      onMouseUp={onMouseUp}
+    >
+      <img width={48} height={48} src={icon} />
+      <span
+        style={{
+          height: "calc(100% - 48px)",
+          width: "100%",
+          fontSize: "16px",
+          textAlign: "center",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          display: "-webkit-box",
+          WebkitLineClamp: 2,
+          WebkitBoxOrient: "vertical",
         }}
-        onDoubleClick={() => createDraggableWindow("test", <p>test</p>)}
-        onClick={() => selectIcon(name)}
       >
-        <img width={48} height={48} src={icon} />
-        <span
-          style={{
-            height: "calc(100% - 48px)",
-            width: "100%",
-            fontSize: "16px",
-            textAlign: "center",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: "vertical",
-          }}
-        >
-          {name}
-        </span>
-      </div>
-    </>
+        {name}
+      </span>
+    </div>
   );
 };
 
