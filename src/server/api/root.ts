c582efba212fa9bf1b9ca0ddcd35256603efbea2
Qłesuga <110699693+Qlesuga@users.noreply.github.com>
2026-02-23 12:13:53 +0100

    fix build

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 //import { emailRouter } from "~/server/api/routers/emailer";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { pingRouter } from "./routers/ping";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +8,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  ping: pingRouter,
   // email: emailRouter,
 });
 
